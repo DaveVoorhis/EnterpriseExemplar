873dba3fbca77dea8258c7fe8397667b0b88e36c
Dave Voorhis <voorhis@gmail.com>
2025-09-18 10:46:09 +0100

    Improved permission categories.

--- java_backend/src/main/java/org/reldb/exemplars/java/backend/enums/Permissions.java
+++ java_backend/src/main/java/org/reldb/exemplars/java/backend/enums/Permissions.java
@@ -8,19 +8,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Permissions {
-    ADMIN("User administration", Categories.ADMIN),
-    FIND_USER("Find user", Categories.VIEW),
-    CHECK_PERMISSION("Verify user has specified permission", Categories.VIEW),
-    GET_ALL_DEMOS("Get all demos", Categories.VIEW),
-    ADD_DEMO("Add a demo", Categories.UPDATE),
-    UPDATE_DEMO("Update a demo", Categories.UPDATE),
-    DELETE_DEMO("Delete a demo", Categories.UPDATE),
-    GET_DEMO("Get a demo", Categories.VIEW);
+    ADMIN("User administration", Categories.USER_ADMIN),
+    FIND_USER("Find user", Categories.USER_ADMIN),
+    CHECK_PERMISSION("Verify user has specified permission", Categories.USER_ADMIN),
+    GET_ALL_DEMOS("Get all demos", Categories.DEMO_VIEW),
+    ADD_DEMO("Add a demo", Categories.DEMO_UPDATE),
+    UPDATE_DEMO("Update a demo", Categories.DEMO_UPDATE),
+    DELETE_DEMO("Delete a demo", Categories.DEMO_UPDATE),
+    GET_DEMO("Get a demo", Categories.DEMO_VIEW);
 
     private static class Categories {
-        private static final String ADMIN = "Admin";
-        private static final String VIEW = "View";
-        private static final String UPDATE = "Update";
+        private static final String USER_ADMIN = "User Admin";
+        private static final String DEMO_VIEW = "Demo View";
+        private static final String DEMO_UPDATE = "Demo Update";
     }
 
     public static List<String> names() {
