4a1e0e66db56fa9a4264e5aae3d2480498bfaada
Dave Voorhis <voorhis@gmail.com>
2025-09-18 11:46:49 +0100

    Update launch scripts to PostgreSQL.

--- java_backend/launch.bat
+++ java_backend/launch.bat
@@ -1,4 +1,4 @@
-set SPRING_DATASOURCE_JDBCURL=jdbc:sqlserver://127.0.0.1:1433;database=main;user=sa;password=pass@Word;encrypt=false
-set SPRING_DATASOURCETWO_JDBCURL=jdbc:sqlserver://127.0.0.1:1433;database=two;user=sa;password=pass@Word;encrypt=false
-set SPRING_DATASOURCETHREE_JDBCURL=jdbc:sqlserver://127.0.0.1:1433;database=three;user=sa;password=pass@Word;encrypt=false
+set SPRING_DATASOURCE_JDBCURL=jdbc:postgresql://postgres_db:5432/main?user=pguser&password=pass@Word
+set SPRING_DATASOURCETWO_JDBCURL=jdbc:postgresql://postgres_db:5432/two?user=pguser&password=pass@Word
+set SPRING_DATASOURCETHREE_JDBCURL=jdbc:postgresql://postgres_db:5432/three?user=pguser&password=pass@Word
 java -Dspring.profiles.active=mockoauth -jar target\JavaBackendExemplar-1.0.0-SNAPSHOT.jar
--- java_backend/launch.sh
+++ java_backend/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export SPRING_DATASOURCE_JDBCURL="jdbc:sqlserver://127.0.0.1:1433;database=main;user=sa;password=pass@Word;encrypt=false"
-export SPRING_DATASOURCETWO_JDBCURL="jdbc:sqlserver://127.0.0.1:1433;database=two;user=sa;password=pass@Word;encrypt=false"
-export SPRING_DATASOURCETHREE_JDBCURL="jdbc:sqlserver://127.0.0.1:1433;database=three;user=sa;password=pass@Word;encrypt=false"
+export SPRING_DATASOURCE_JDBCURL="jdbc:postgresql://postgres_db:5432/main?user=pguser&password=pass@Word"
+export SPRING_DATASOURCETWO_JDBCURL="jdbc:postgresql://postgres_db:5432/two?user=pguser&password=pass@Word"
+export SPRING_DATASOURCETHREE_JDBCURL="jdbc:postgresql://postgres_db:5432/three?user=pguser&password=pass@Word"
 java -Dspring.profiles.active=mockoauth -jar target/JavaBackendExemplar-1.0.0-SNAPSHOT.jar
